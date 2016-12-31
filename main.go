46376da7a9d24b005aea781468a9f99d63a9a6f4
Shuichi Nagao <ngo38114@gmail.com>
2016-12-31 11:59:46 +0900

    add html template

--- main.go
+++ main.go
@@ -5,29 +5,54 @@ import (
     "fmt"
     "log"
     "net/http"
+    "html/template"
 
     "github.com/garyburd/redigo/redis"
 )
 
 func main() {
+    http.HandleFunc("/", viewHandler)
+
+    log.Fatal(http.ListenAndServe(":5000", nil))
+}
+
+type Page struct {
+    Count int64
+}
+
+func viewHandler (w http.ResponseWriter, r *http.Request) {
     redi, err := redis.Dial("tcp", "redis:6379")
     if err != nil {
         log.Fatal(err)
     }
-    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-        res, err := redi.Do("incr", "counter")
-        if err != nil {
-            w.WriteHeader(500)
-            w.Write([]byte(err.Error()))
-            return
-        }
 
-        if res, ok := res.(int64); ok {
-            w.Write([]byte(fmt.Sprintf("counter: %d", res)))
-        } else {
-            w.WriteHeader(500)
-            w.Write([]byte("unexpected value"))
-        }
-    })
-    log.Fatal(http.ListenAndServe(":5000", nil))
+    res, err := redi.Do("incr", "counter")
+    if err != nil {
+        w.WriteHeader(500)
+        w.Write([]byte(err.Error()))
+
+        return
+    }
+
+    if res, ok := res.(int64); ok {
+        fmt.Print(res)
+        //w.Write([]byte(fmt.Sprintf("counter: %d", res)))
+        createDomFromTemplate(w, res)
+    } else {
+        w.WriteHeader(500)
+        w.Write([]byte("unexpected value"))
+    }
+}
+
+func createDomFromTemplate (w http.ResponseWriter, res int64) {
+    tmpl, err := template.ParseFiles("view.html")
+    if err != nil {
+        panic(err)
+    }
+
+    page := Page{Count: res}
+    err = tmpl.Execute(w, page)
+    if err != nil {
+        panic(err)
+    }
 }
